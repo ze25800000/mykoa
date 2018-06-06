d7115419053a9f34cb929a5a8983fecce11c07c5
ze25800000 <ze258100000@sina.com>
2018-06-06 11:46:13 +0800

    异步compose原理

--- compose.js
+++ compose.js
@@ -1,23 +1,45 @@
-function add(x, y) {
-    return x + y
+async function fn1(next) {
+    console.log('fn1')
+    await next()
+    console.log('fn1 end')
 }
 
-function double(z) {
-    return z * 2
+async function fn2(next) {
+    console.log('fn2')
+    await next()
+    await delay()
+    console.log('fn2 end')
 }
 
-const middlewares = [add, double]
-let len = middlewares.length
+function fn3() {
+    console.log('fn3')
+}
 
-function compose(middle) {
-    return (...args) => {
-        let res = middle[0](...args)
-        for (let i = 1; i < len; i++) {
-            res = middle[i](res)
+function delay() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+    })
+}
+
+
+function compose(middlewares) {
+    return function () {
+        return dispatch(0)
+
+        function dispatch(i) {
+            let fn = middlewares[i]
+            if (!fn) {
+                return Promise.resolve()
+            }
+            return Promise.resolve(fn(function next() {
+                return dispatch(i + 1)
+            }))
         }
-        return res
     }
 }
 
-const fn = compose(middlewares)
-console.log(fn(1, 2))
+const middlewares = [fn1, fn2, fn3]
+const finalFn = compose(middlewares)
+finalFn()
